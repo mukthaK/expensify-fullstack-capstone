693fea8c29c8b091c62967026e763710ad8bc9c1
Muktha Krishnaswamy <muktha.1689@gmail.com>
2018-09-19 17:22:25 -0700

    test cases written

--- test/test.js
+++ test/test.js
@@ -7,6 +7,7 @@ const {
 
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+const User = require('./models/user');
 const Friend = require('./models/friend');
 const Bill = require('./models/bill');
 //var entry = require('../models/entry.js');
@@ -16,6 +17,69 @@ const Bill = require('./models/bill');
 var should = chai.should();
 
 chai.use(chaiHttp);
+
+// --------------- Test User Endpoints ---------------
+
+describe('User API resource', function () {
+
+    before(function () {
+        return runServer(TEST_DATABASE_URL)
+            .then(console.log('Running server'))
+            .catch(err => console.log({
+                err
+            }));
+    });
+
+    beforeEach(function () {
+        return seedUserData();
+    });
+
+    // Test create a new user
+    it('should create a new user', function () {
+        return chai.request(app)
+            .post('/users/create')
+            .send({
+                email: "test@gmail.com",
+                password: "testPassword"
+            })
+            .then(function (res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.include.keys('email', 'password');
+                res.body.email.should.equal(newUser.email);
+                res.body.password.should.not.equal(newUser.password);
+                res.body._id.should.not.be.null;
+            });
+    });
+
+    // Change user password
+    it('should update user password', function () {
+        //const updateUser = generateUser();
+        const updatePw = "testPassword";
+
+        return chai.request(app)
+            .put(`/update-password/:loggedinUser`)
+            .send(updatePw)
+            .then(function (res) {
+                res.should.have.status(200);
+                return User.findById(updateUser.id);
+            })
+            .then(function (user) {
+                user.password.should.not.be.null;
+                user.password.should.not.equal(updatePw);
+            })
+    });
+
+
+    afterEach(function () {
+        return tearDownDb();
+    });
+
+    after(function () {
+        return closeServer();
+    });
+});
+
 
 // Friend endpoints check - test cases
 describe('Friend API', function () {
